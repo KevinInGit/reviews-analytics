3a7e477ca8810d86772ea17637e74de1310aff98
kevinlee <185157187@qq.com>
2018-11-27 23:17:07 +0800

    calc acg length of reviews

--- reviews-analytics.py
+++ reviews-analytics.py
@@ -9,6 +9,10 @@ with open('/Users/kevin/Desktop/read/food.txt', 'r') as f:
         if count % 1000 == 0:
             print(len(data))
 
-print(len(data))
-print(data)
+print('文档读取完毕，共有', len(data), '个资料')
 
+sum_len = 0
+for d in data:
+    sum_len += len(d)
+
+print('平均每个字符串长度是', sum_len/len(data))
