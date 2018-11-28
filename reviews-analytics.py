a3e8aabb88052f9eb73025418097f8276656099b
kevinlee <185157187@qq.com>
2018-11-28 11:27:55 +0800

    add filtering

--- reviews-analytics.py
+++ reviews-analytics.py
@@ -8,11 +8,17 @@ with open('/Users/kevin/Desktop/read/food.txt', 'r') as f:
         count += 1
         if count % 1000 == 0:
             print(len(data))
-
 print('文档读取完毕，共有', len(data), '个资料')
 
 sum_len = 0
 for d in data:
     sum_len += len(d)
-
 print('平均每个字符串长度是', sum_len/len(data))
+
+#筛选长度小于100的留言
+new = []
+for d1 in data:   #d 是字符串  一个个留言  ；  data是留言总和  一个列表
+    if len(d1) < 100:
+        new.append(d1)
+print('一共有', len(new), '留言长度小于100')
+print(new[0])
